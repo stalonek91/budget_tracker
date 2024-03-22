cd9a40d7f6cc131dbc4cbf612fa1a63090a50baf
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2024-03-22 16:59:30 +0100

    Created sections in gui and created new function for it's handling

--- tracker.py
+++ tracker.py
@@ -6,10 +6,48 @@ from sqlalchemy.orm import sessionmaker
 import tkinter as tk
 from tkinter import ttk
 
+def clear_content():
+    print(f'Function clear content triggered')
+    for widget in main_content.winfo_children():
+        widget.destroy()
+
+
+def create_fields(labels: list):
+    entries = {}
+
+    for i, text in enumerate(labels):
+        label = tk.Label(main_content, text=text, bg=main_content_bg, fg="black")
+        label.grid(row=i, column=0, sticky='e', padx=5, pady=5)
+
+        entry = tk.Entry(main_content)
+        entry.grid(row=i, column=1, sticky="ew", padx=5, pady=5)
+        entries["text"] = entry 
+
+
+def show_saving():
+    print(f'Function show saving')
+    clear_content()
+
+    labels_text = ['Date', 'Product', 'Price', 'Account name']
+    create_fields(labels_text)
+
+
+def show_accounts():
+    clear_content()
+    #TODO: stworzyc liste accounts na bazie wpisow z bazy danych
+
+
+def show_expense():
+    print(f'We are in show expense function')
+    clear_content()
+
+    labels_text = ['Product', 'Price', 'Category', 'Importance', 'Account Name']
+    create_fields(labels_text)
+
 
 root = tk.Tk()
 root.title("Budget Tracker")
-
+root.geometry("1000x500")
 
 
 sidebar = ttk.Frame(root, width=200, height=500)
@@ -17,13 +55,13 @@ sidebar.pack(expand=False, fill='y', side='left', anchor='nw')
 
 
 # Create buttons and add them to the sidebar frame
-expense_button = ttk.Button(sidebar, text="Expense")
+expense_button = ttk.Button(sidebar, text="Expense", command=show_expense)
 expense_button.pack(fill='x')
 
-saving_button = ttk.Button(sidebar, text="Saving")
+saving_button = ttk.Button(sidebar, text="Saving", command=show_saving)
 saving_button.pack(fill='x')
 
-accounts_button = ttk.Button(sidebar, text="Accounts")
+accounts_button = ttk.Button(sidebar, text="Accounts",command=show_accounts)
 accounts_button.pack(fill='x')
 
 # Main content area
@@ -49,18 +87,7 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# new_accounts = [
-#     Accounts(account="Pekao", balance = 14000.00, date=today_date),
-#     Accounts(account="Revolut", balance = 11000.00, date=today_date),
-#     Accounts(account="Etoro", balance = 5400.00, date=today_date),
-#     Accounts(account="Xtb", balance = 500.00, date=today_date),
-#     Accounts(account="Cash", balance = 3300.00, date=today_date),
-#     Accounts(account="ViennaLife", balance = 58000.00, date=today_date),
-# ]
 
-# session.add_all(new_accounts)
-# session.commit()
-# session.close()
 
 
 root.mainloop()
